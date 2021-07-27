4f3e53e150714de4b0403b17bda0d9c9e0702beb
Ankur Sinha (Ankur Sinha Gmail) <neuro-sig@lists.fedoraproject.org>
2021-07-27 10:36:24 +0100

    feat: add GH cron action

--- update.sh
+++ update.sh
@@ -6,29 +6,72 @@
 #
 # build the planet and commit
 
-if ! command -v git; then
-    echo "Git is required."
-    exit -1
-else
-    git pull --force
-fi
-
-if ! command -v pluto; then
-    echo "pluto is not installed. Please install using 'gem install pluto'"
-    exit -1
-fi
-
-pluto --err build planet.ini -t neuroscience -o docs
-
-# Rename file
-pushd docs
-    if [ -f "planet.neuroscience.html" ]; then
-        mv planet.neuroscience.html index.html
+check_git () {
+    if ! command -v git; then
+        echo "Git is required."
+        exit -1
+    else
+        git config user.email "neuro-sig@lists.fedoraproject.org"
+        git config user.name "neurofedorabot"
     fi
-popd
+}
 
-git add .
-git commit -m "Regenerated"
-git push -u origin master
+refresh_repo () {
+    git pull --force
+}
 
-exit 0
+check_pluto () {
+    if ! command -v pluto; then
+        mkdir ~/bin/
+        gem install --user-install pluto rss -n ~/bin/ || exit -1
+    fi
+}
+
+rebuild_planet () {
+    ~/bin/pluto --err build planet.ini -t neuroscience -o docs || exit -1
+    # Rename file
+    pushd docs
+        if [ -f "planet.neuroscience.html" ]; then
+            mv planet.neuroscience.html index.html
+        fi
+    popd
+}
+
+commit_update () {
+    git add .
+    git commit -m ":rocket: Regenerated"
+    git push -u origin master
+}
+
+usage () {
+    echo "update.sh: update the planet"
+    echo "Usage: update.sh -[lg]"
+    echo
+    echo "-g: GitHub actions build"
+    echo "-l: local build"
+}
+
+while getopts "lg" OPTION
+do
+    case $OPTION in
+        l)
+            check_git
+            check_pluto
+            refresh_repo
+            rebuild_planet
+            commit_update
+            exit 0
+            ;;
+        g)
+            check_git
+            check_pluto
+            rebuild_planet
+            commit_update
+            exit 0
+            ;;
+        ?)
+            usage
+            exit 0
+            ;;
+    esac
+done
